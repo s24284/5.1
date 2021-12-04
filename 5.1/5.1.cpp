2a4df66b914d58f1adceb6a85c05db76b8b22e5b
Anna Prymas <s24284@pjwstk.edu.pl>
2021-12-04 13:35:03 +0100

    Dodaj pliki projektów.

---
+++ 5.1/5.1.cpp
@@ -0,0 +1,36 @@
+﻿// 5.1.cpp : 
+//
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+int main()
+{
+    int rozmiartablicy, najwyższa = INT_MIN, indeksnaj;
+
+
+    std::cout << "Stworze dla ciebie tablice liczb calkowitych. Podaj liczbe elementow, z ktorych ma sie skladac:\n";
+    std::cin >> rozmiartablicy;
+
+    std::vector<int> tabliczka(rozmiartablicy);
+
+    for (int i = 0; i < rozmiartablicy; i++)
+    {
+        std::cout << "Podaj mi " << i + 1 << " liczbe, ktora trafi do tablicy:\n";
+        std::cin >> tabliczka[i];
+
+        if (tabliczka[i] > najwyższa)
+        {
+            najwyższa = tabliczka[i];
+            indeksnaj = i;
+        }
+
+    }
+
+    std::cout << "Najwieksza liczba z podanych do tablicy jest: " << najwyższa << "\n Znajduje sie ona w : " << indeksnaj << " indeksie tablicy"; 
+
+    return 0;
+
+}
+
